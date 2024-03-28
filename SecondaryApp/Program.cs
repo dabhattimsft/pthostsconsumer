731b3602ac277246b02fd76195a94bca74381d87
Darshak Bhatti <dabhatti@micorsoft.com>
2024-03-28 15:32:21 -0700

    cp

---
+++ SecondaryApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.UI.Dispatching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SecondaryApp
+{
+    public static class Program
+    {
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+        private static App? _app;
+#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            WinRT.ComWrappersSupport.InitializeComWrappers();
+
+            Microsoft.UI.Xaml.Application.Start((p) =>
+            {
+                var dispatcherQueue = DispatcherQueue.GetForCurrentThread();
+                var context = new DispatcherQueueSynchronizationContext(dispatcherQueue);
+                SynchronizationContext.SetSynchronizationContext(context);
+                _app = new App();
+            });
+        }
+    }
+}
